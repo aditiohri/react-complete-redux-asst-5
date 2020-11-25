4d1a6e461c9410ff7d92638ae61cbecc8ead2f2e
Aditi Ohri <didiohri@gmail.com>
2020-11-24 20:59:16 -0600

    remove references to handlers in Persons.js

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -1,55 +1,44 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actionTypes from '../store/actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionTypes from "../store/actions";
 
-import Person from '../components/Person/Person';
-import AddPerson from '../components/AddPerson/AddPerson';
+import Person from "../components/Person/Person";
+import AddPerson from "../components/AddPerson/AddPerson";
 
 class Persons extends Component {
-    state = {
-        persons: []
-    }
+  state = {
+    persons: [],
+  };
 
-    // personAddedHandler = () => {
-        
-    //     this.setState( ( prevState ) => {
-    //         return { persons: prevState.persons.concat(newPerson)}
-    //     } );
-    // }
-
-    // personDeletedHandler = (personId) => {
-    //     this.setState( ( prevState ) => {
-    //         return { persons: prevState.persons.filter(person => person.id !== personId)}
-    //     } );
-    // }
-
-    render () {
-        return (
-            <div>
-                <AddPerson personAdded={this.props.addPersonHandler} />
-                {this.state.persons.map(person => (
-                    <Person 
-                        key={person.id}
-                        name={person.name} 
-                        age={person.age} 
-                        clicked={() => this.props.deletePersonHandler(person.id)}/>
-                ))}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <AddPerson personAdded={this.props.addPersonHandler} />
+        {this.props.storedPersons.map((person) => (
+          <Person
+            key={person.id}
+            name={person.name}
+            age={person.age}
+            clicked={() => this.props.deletePersonHandler(person.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-        persons: state.persons
-    }
-}
+const mapStateToProps = (state) => {
+  return {
+    storedPersons: state.persons,
+  };
+};
 
-const mapDispatchToProps = dispatch  => {
-    return {
-        addPersonHandler: () => dispatch({ type: actionTypes.ADD }),
-        deletePersonHandler: (personId) => dispatch({ type: actionTypes.DELETE, personId: personId })
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addPersonHandler: () => dispatch({ type: actionTypes.ADD }),
+    deletePersonHandler: (personId) =>
+      dispatch({ type: actionTypes.DELETE, personId: personId }),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Persons);
